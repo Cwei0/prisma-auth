15df4bf48a616bb22695cbbc6862bba78ba9b8cd
Cwei0 <109186036+Cwei0@users.noreply.github.com>
2023-05-05 16:13:41 +0100

    feat: created login controller and jwt

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,8 +1,23 @@
-import { hashSync } from "bcrypt";
+import { compare, hashSync } from "bcrypt";
 import { env } from "process";
+import { createSessionInput } from "../schemas/session.schema"
+import db from "../config/db";
 
 export function hashPassword(input: string): string {
     const rounds = Number(env.SALT)
     const hashed = hashSync(input, rounds)
     return hashed
+}
+
+
+export async function validatePassword({email, password} : createSessionInput['body']) {
+    const user = await db.user.findUniqueOrThrow({
+        where: {
+            email
+        },
+    })
+    const isValid = await compare(password, user.password)
+    if (!isValid) return false
+
+    return user
 }
